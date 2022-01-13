91e9209e401e25573bfe20c25863e1e9d6884ed3
Fatih <95652578+fatihqaz@users.noreply.github.com>
2022-01-14 01:02:02 +0300

    edit

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
+let label = document.getElementById("Label")
+let button = document.getElementById("Button")
+let body = document.body
+let root = document.querySelector(':root');
 
 
+function rnd() {
+    return Math.floor(Math.random() * 16).toString(16)
+}
 
-        let colorLabel = document.getElementById("colorLabel")
-        let button = document.getElementById("colorButton")
-        let body = document.body
-        let root = document.querySelector(':root');
-
-
-        function r() {
-            return Math.floor(Math.random() * 16).toString(16)
-        }
-
-        button.onclick = function name(params) {
-            let color = "#" + r() + r() + r() + r() + r() + r()
-            body.classList.replace("color","color")
-            root.style.setProperty('--color', color)
-            colorLabel.innerHTML = color.toUpperCase()
-            console.log(color)
-        }
+button.onclick = () => {
+    let color = "#" + rnd() + rnd() + rnd() + rnd() + rnd() + rnd()
+    body.classList.replace("color", "color")
+    root.style.setProperty('--color', color)
+    label.innerHTML = color.toUpperCase()
+    console.log(color)
+}
